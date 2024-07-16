d11c08c400aaae05d05cc910a59332b2cf36951a
Douglas de Oliveira Ialamov <douglas.ialamov@gmail.com>
2024-07-16 14:18:40 -0300

    Excluding unused codes.

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -19,21 +19,5 @@ const AppDataConnection = new DataSource({
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
 })
 
-
-// const connection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   database: process.env.MYSQL_DATABASE,
-//   password: process.env.MYSQL_PASSWORD,
-//   port: Number(process.env.MYSQL_PORT)
-// }).then(() => {
-//   Logging.info('DB connected successfully!');
-//   StartServer();
-// })
-// .catch(err => {
-//   Logging.error('Error: ');
-//   Logging.error(err);
-// })
-
 export default AppDataConnection
 
