49b3603b4a77a839f58140643cd002a077ba29b7
Douglas de Oliveira Ialamov <douglas.ialamov@gmail.com>
2024-07-16 14:14:12 -0300

    Changing connection to use typeorm for database.

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,12 +1,39 @@
 import mysql from 'mysql2'
 import * as dotenv from 'dotenv'
+import Logging from './logging.js';
+import { DataSource } from 'typeorm';
 
 dotenv.config()
 
-const connection = mysql.createConnection({
+const __dirname = process.cwd()
+
+const AppDataConnection = new DataSource({
+  type: 'mysql',
   host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
+  port: Number(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: Number(process.env.MYSQL_PORT)
+  // database: process.env.MYSQL_DATABASE,
+
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
 })
+
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   database: process.env.MYSQL_DATABASE,
+//   password: process.env.MYSQL_PASSWORD,
+//   port: Number(process.env.MYSQL_PORT)
+// }).then(() => {
+//   Logging.info('DB connected successfully!');
+//   StartServer();
+// })
+// .catch(err => {
+//   Logging.error('Error: ');
+//   Logging.error(err);
+// })
+
+export default AppDataConnection
+
