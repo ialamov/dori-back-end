4d8e688e4861c41d5a470f07da46c99aa3066a9c
Douglas de Oliveira Ialamov <douglas.ialamov@gmail.com>
2024-07-17 10:33:48 -0300

    Added the Logging class, which put colors in the logs.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
+import "reflect-metadata"
 import app from "./app.js"
 import AppDataConnection from "./utils/connection.js"
 import * as dotenv from 'dotenv'
+import Logging from "./utils/logging.js"
 
 dotenv.config()
 
 const port = 8300
 
 AppDataConnection.initialize().then(() => {
+  Logging.info('DB connected successfully!');
+})
+.then(() => {
   app.listen(port, () => {
-    console.log('The server is running on port 3000')
+    Logging.info(`The server is running on port ${port}`)
   })
+})
+.catch(err => {
+  Logging.error('Error: ');
+  Logging.error(err);
 })
 
 
