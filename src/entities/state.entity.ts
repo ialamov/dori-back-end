f66fa0d45a31ef4c47f5482e3ed87f502d2b5d1e
Douglas de Oliveira Ialamov <douglas.ialamov@gmail.com>
2024-07-17 11:44:39 -0300

    Implements the entity state.

---
+++ src/entities/state.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class StateEntity {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: 'int',
+  })
+  id!: number
+
+  @Column({
+    name: 'initials',
+    type: 'varchar',
+    length: 2,
+    nullable: false,
+    unique: true
+  })
+  initials!: string
+
+  @Column({
+    name: 'state_full_name',
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true
+  })
+  stateFullName!: string
+
+  @Column({
+    name: 'code',
+    type: 'varchar',
+    nullable: false,
+    unique: true
+  })
+  code!: number
+}
