f145ebd1cf178c108c702d1787b0328e272045eb
Douglas de Oliveira Ialamov <douglas.ialamov@gmail.com>
2024-07-17 11:58:59 -0300

    Implements the entity city.

---
+++ src/entities/city.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { StateEntity } from "./state.entity.js";
+
+@Entity()
+export class CityEntity {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: 'int'
+  })
+  id!: number
+
+  @ManyToOne(() => StateEntity, {
+    nullable: false,
+    eager: true
+  })
+  @JoinColumn({
+    name: 'state'
+  })
+  state!: StateEntity
+
+  @Column({
+    name: 'city',
+    type: 'varchar',
+    nullable: false,
+    unique: true
+  })
+  city!: string
+}
